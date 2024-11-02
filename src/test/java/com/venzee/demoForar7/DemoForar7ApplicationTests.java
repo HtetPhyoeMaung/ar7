1778121e37f95d54e228bfd8e2c0f2f3543dc304
HtetPhyoeMaung <htetphyoemaung@edusn.co.uk>
2024-11-02 14:29:37 +0630

    first commit

---
+++ src/test/java/com/venzee/demoForar7/DemoForar7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venzee.demoForar7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForar7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
